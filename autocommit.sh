b8774dd85160a0dc6dbf91691bb6e7566a44857a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-27 22:21:08 +0600

    added some fix but didi not work

--- autocommit.sh
+++ autocommit.sh
@@ -1,15 +1,15 @@
-PATH="$(pwd)"
+PATH="/Users/sanjarabakirov/GH_Computer_systems/GH_Computer_systems_and_industrial_programming-main"
 
 cd "$PATH" || exit
 
 echo "Control all changes in $PATH..."
 
-while true, do
+while true; do
     if ! git diff --quiet || ! git diff --cached --quiet; then
-	echo "Changes defined! Commit and push..."
-	git add .
-	git commit -m "auto commit $(date)"
-	git push
+		echo "Changes defined! Commit and push..."
+		git add .
+		git commit -m "auto commit $(date)" || true
+		git push
     fi
     sleep 10
 done
--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ echo "${x}"
 echo "${x:0:7}"
 echo "${#x}"
 
-echo "Hello Git
+echo "Hello Git"
+echo 'Hello Git 2'
 
 echo "String testing" completed"
