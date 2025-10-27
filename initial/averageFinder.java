70b5ea3f47424631e1058ea0013406ac4375cca8
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-27 14:00:17 +0600

    Added new Java and syntax

--- initial/averageFinder.java
+++ initial/averageFinder.java
@@ -3,6 +3,8 @@ package sam.classIntro
 public class AverageFinder {
     public static void main(String[] args){
         System.out.printLn("AverageFinder");
+        double avg = findAverage(args);
+        System.out.printLn("The average is " + avg)
 
 
 }
