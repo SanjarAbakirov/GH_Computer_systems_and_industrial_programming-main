c28ae9a0269c2c93c909c7adce59802861c4307a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-27 14:04:51 +0600

    added many new commands for Java file

--- initial/averageFinder.java
+++ initial/averageFinder.java
@@ -4,7 +4,18 @@ public class AverageFinder {
     public static void main(String[] args){
         System.out.printLn("AverageFinder");
         double avg = findAverage(args);
-        System.out.printLn("The average is " + avg)
+        System.out.printLn("The average is " + avg);
+    }
 
+    static double findAverage(String[] input){
+        double result = 0;
+        if (input.length == 0) {
+            return result;
+        }
+        
+        for (String s : input) {
+            result += Integer.parseInt(s);
+        }
 
-}
+        return result/ input.length;
+    }
